64deb75dfd64747ada24bb63911e21b58ea3f1ad
Dariem Macias Mora <dariem.carlos@hotmail.com>
2020-06-06 16:32:42 -0500

    adding a component so users can register on site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'; // using HttpClientModule Class from @angular/common/http modules
-import {FormsModule} from '@angular/forms/';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
-import { from } from 'rxjs';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { from } from 'rxjs';
 
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule, // moduleimportedtocreatehttprequest
+      HttpClientModule,
       FormsModule
+      // module importedtocreatehttprequest\\nFormsModule
    ],
    providers: [
       AuthService
