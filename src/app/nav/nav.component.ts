2ef882a4dc43be6a6a12ca0b57c2a7139a610373
Dariem Macias Mora <dariem.carlos@hotmail.com>
2020-06-06 14:14:41 -0500

    Using *ngIf condicionally show HTML

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -4,22 +4,31 @@ import { AuthService } from '../_services/auth.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-model: any = {};
-  constructor(private authService: AuthService) { }
+  model: any = {};
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
-  }
-  login(){
+  ngOnInit() {}
+  login() {
     this.authService.login(this.model).subscribe(
-      next => {
+      (next) => {
         console.log('Logged in successfully');
       },
-      error => {
-        console.log('Logged in error');
+      (error) => {
+        console.log('Login failed');
       }
-      );
+    );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
   }
 }
