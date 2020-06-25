89ce03be257760f344e35e44f7b4a2764dec0b9b
Dariem Macias Mora <dariem.carlos@hotmail.com>
2020-06-25 13:16:45 -0500

    Refactoring code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { RegisterComponent } from './register/register.component';
 import { from } from 'rxjs';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { ListsComponent } from './lists/lists.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { UserService } from './_services/user.service';
