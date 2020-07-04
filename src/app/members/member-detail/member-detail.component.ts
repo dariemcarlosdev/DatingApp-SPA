19568867a70d715c6a856f9d5346c4a51f532cd2
Dariem Macias Mora <dariem.carlos@hotmail.com>
2020-07-04 15:15:32 -0500

    ngx-gallery css issue solved

--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -31,8 +31,7 @@ export class MemberDetailComponent implements OnInit {
         height: '500px',
         imagePercent: 100,
         thumbnailsColumns: 4,
-        imageAnimation: NgxGalleryAnimation.Slide,
-        preview: false
+        imageAnimation: NgxGalleryAnimation.Slide
       }
     ];
     this.galleryImages = this.getImages();
